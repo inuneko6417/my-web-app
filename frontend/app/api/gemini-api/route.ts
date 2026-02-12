c5474685ff141de22963e12bf84d3297897ca240
inuneko6417 <sao64179060@gmail.com>
2026-02-12 22:22:25 +0900

    gemini-flash-2.0-liteに修正

--- frontend/app/api/gemini-api/route.ts
+++ frontend/app/api/gemini-api/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: Request) {
     const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
 
     const response = await ai.models.generateContent({
-        model: "gemini-2.0-flash",
+        model: "gemini-2.0-flash-lite",
         contents: prompt_post,
       });
 
