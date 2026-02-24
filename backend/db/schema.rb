2a40441b0b95811971d3b01fb083290640411baa
inuneko6417 <sao64179060@gmail.com>
2026-02-24 22:50:35 +0900

    食材のデータベース作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_19_120421) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_23_113934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.string "quantity"
+    t.bigint "recipe_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -24,4 +33,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_19_120421) do
     t.string "youtube_url"
     t.index ["video_id"], name: "index_recipes_on_video_id", unique: true
   end
+
+  add_foreign_key "ingredients", "recipes"
 end
