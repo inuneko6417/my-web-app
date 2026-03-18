c6d670ababfcbce79d42fcb24543ec81348d499e
inuneko6417 <sao64179060@gmail.com>
2026-03-18 11:00:39 +0900

    テストでも使えるように修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://recipe-tube-azure.vercel.app'
+    origins 'http://localhost:3000', /https:\/\/.*\.vercel\.app/
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
